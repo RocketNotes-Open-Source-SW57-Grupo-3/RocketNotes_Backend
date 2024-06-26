14d06c262679ea8a6696a8c1e09195a50e68ad9b
GoodFries04 <114363212+GoodFries04@users.noreply.github.com>
2024-06-26 08:10:16 -0500

    fix:  bad connections

--- src/main/java/com/fivestars/rocketnotes/admins/domain/model/commands/UpdateTeacherCommand.java
+++
@@ -1,4 +0,0 @@
-package com.fivestars.rocketnotes.admins.domain.model.commands;
-
-public record UpdateTeacherCommand(Long id, String firstName, String paternalLastName, String maternalLastName, String dni, String phone, String email) {}
-
